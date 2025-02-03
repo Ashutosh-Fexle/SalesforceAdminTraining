6c03580f30c2a0062e890d823303a3dac6b7b2a7
Ashutosh-Fexle <ashutosh.tiwari@fexle.com>
2025-02-03 23:24:49 +0530

    LWC Session-3 Assignment Commit

---
+++ force-app/main/default/lwc/messangerA/messangerA.js
@@ -0,0 +1,38 @@
+import { LightningElement, track , wire } from 'lwc';
+import { publish, MessageContext, subscribe } from 'lightning/messageService';
+import MESSAGE_CHANNEL from '@salesforce/messageChannel/MessageChannel_A_B__c';
+
+export default class MessengerA extends LightningElement {
+    @track messageToSend = '';
+    @track receivedMessage = '';
+
+    @wire(MessageContext) messageContext;
+
+    connectedCallback() 
+    {
+        this.subscribeToMessageChannel();
+    }
+
+    handleInputChange(event) 
+    {
+        this.messageToSend = event.target.value;
+    }
+
+    sendMessage() 
+    {
+        const message = { message: this.messageToSend };
+        publish(this.messageContext, MESSAGE_CHANNEL, message);
+    }
+
+    subscribeToMessageChannel() 
+    {
+        subscribe(this.messageContext, MESSAGE_CHANNEL, (message) => {
+            this.handleMessage(message);
+        });
+    }
+
+    handleMessage(message) 
+    {
+        this.receivedMessage = message.message;
+    }
+}
