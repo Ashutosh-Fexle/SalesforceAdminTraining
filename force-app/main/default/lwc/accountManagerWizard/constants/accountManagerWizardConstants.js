a3079c7793d0fec2247a786eeb78296655186f16
Ashutosh-Fexle <ashutosh.tiwari@fexle.com>
2025-02-10 10:13:29 +0530

    LWC Session-4 Assignment Commit

---
+++ force-app/main/default/lwc/accountManagerWizard/constants/accountManagerWizardConstants.js
@@ -0,0 +1,20 @@
+const ACCOUNT_RELATED_CONTACTS_COLUMNS = [
+    { 
+        label: 'Id' , 
+        fieldName: 'Id'
+    },
+    { 
+        label: 'First Name' , 
+        fieldName: 'FirstName'
+    },
+    { 
+        label: 'Last Name' , 
+        fieldName: 'LastName'
+    },
+    { 
+        label: 'Phone' , 
+        fieldName: 'Phone'
+    },
+];
+
+export  {ACCOUNT_RELATED_CONTACTS_COLUMNS};
